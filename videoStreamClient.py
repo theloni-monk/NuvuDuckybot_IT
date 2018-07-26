3aab2696657242257d586485ba42d90d6e515769
iahuang <iahuang@users.noreply.github.com>
2018-07-26 19:57:26 -0400

    Test

--- videoStreamClient.py
+++ videoStreamClient.py
@@ -1,5 +1,23 @@
 import socket
+import pickle
+import io
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('18.111.87.85', 444))
+
+def recv(size=1024):
+    data = bytearray()
+    while 1:
+        buffer = s.recv(1024)
+        if len(buffer)>0:
+            data+=buffer
+        else:
+            return data
+
+while 1:
+    b = io.BytesIO(recv())
+    img = pickle.load(b)
+    cv2.imshow(img)
+    if cv2.waitKey(1) == 27:
+        break  # esc to quit
 
--- videoStreamServer.py
+++ videoStreamServer.py
@@ -1,5 +1,6 @@
 import socket
 import cv2
+import camera
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('', 444))
@@ -10,5 +11,12 @@ while True:
     conn, addr = s.accept()
     print('Connected with ' + addr[0] + ':' + str(addr[1]))
     break
+
+cam = camera.Camera()
+
+while True:
+    b = io.BytesIO()
+    img = pickle.dump(cam.image,b)
+    s.send(b.getvalue())
     
 s.close()
