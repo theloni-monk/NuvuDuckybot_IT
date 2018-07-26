5c7ad40166e2635e745f97dfa156044495ffbbeb
thatguy1234510 <theoac2009@outlook.com>
2018-07-26 10:16:40 -0400

    id

--- driveStraightAnswer.py
+++ driveStraightAnswer.py
@@ -11,8 +11,6 @@ lmotor = mh.getMotor(1)
 rmotor = mh.getMotor(2)
 
 # recommended for auto-disabling motors on shutdown!
-
-
 def turnOffMotors():
     mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
     mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
@@ -22,12 +20,6 @@ def turnOffMotors():
 
 atexit.register(turnOffMotors)
 
-
-# Complete this function so:
-# 1. values in the range 1 to 32768 make the motor spin forward faster and faster.
-# 2. values in the range -1 to -32768 make the motor spin backward faster and faster.
-# 3. any value equal to 0 makes the motor BRAKE.
-# 4. any values less than -32768 and greater than 32768 use the max speed in the right direction.
 def runMotor(motor, speed):
     """ motor - the motor object to control.
         speed - a number from -32768 (reverse) to 32768 (forward) """
@@ -35,13 +27,19 @@ def runMotor(motor, speed):
     if 1 <= speed <= 32768:
         mh.forward
         motor.setSpeed(int(speed*(255/32768)))
+    elif speed>32768:
+        mh.forward
+        motor.setSpeed(255)
     elif -1 < speed < 1:
         motor.setSpeed(0)
         mh.BRAKE
-    elif -32768 <= speed <= -1
+    elif -32768 <= speed <= -1:
         mh.backward
         motor.setspeed(int(-speed*(255/32768)))
+    elif speed<-32768:
+        mh.backward
+        motor.setspeed(255)
 
-
+#drive both motors straight for debuging
 runMotor(lmotor, 32767)
 runMotor(rmotor, 32767)
