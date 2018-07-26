8f95b3dee4f26f24e7e2fa8fa51034be11cb2aab
thatguy1234510 <theoac2009@outlook.com>
2018-07-26 09:54:14 -0400

    wrote run motor func

--- driveStraightAnswer.py
+++ driveStraightAnswer.py
@@ -11,14 +11,16 @@ lmotor = mh.getMotor(1)
 rmotor = mh.getMotor(2)
 
 # recommended for auto-disabling motors on shutdown!
+
+
 def turnOffMotors():
-  mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
-  mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
-  mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
-  mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
+    mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
+    mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
+    mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
+    mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
+
 
 atexit.register(turnOffMotors)
-
 
 
 # Complete this function so:
@@ -27,17 +29,19 @@ atexit.register(turnOffMotors)
 # 3. any value equal to 0 makes the motor BRAKE.
 # 4. any values less than -32768 and greater than 32768 use the max speed in the right direction.
 def runMotor(motor, speed):
-  """ motor - the motor object to control.
-      speed - a number from -32768 (reverse) to 32768 (forward) """
-  # COMPLETE THIS FUNCTION!
-  pass # delete this line when you start writing your code.
+    """ motor - the motor object to control.
+        speed - a number from -32768 (reverse) to 32768 (forward) """
+    # COMPLETE THIS FUNCTION!
+    if 1 <= speed <= 32768:
+        mh.forward
+        motor.setSpeed(int(speed*(255/32768)))
+    elif -1 < speed < 1:
+        motor.setSpeed(0)
+        mh.BRAKE
+    elif -32768 <= speed <= -1
+        mh.backward
+        motor.setspeed(int(-speed*(255/32768)))
 
 
-runMotor(lmotor,32767)
-runMotor(rmotor,32767)
-
-
-
-  
-
-
+runMotor(lmotor, 32767)
+runMotor(rmotor, 32767)
