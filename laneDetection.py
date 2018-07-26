f5b81227dd6fc8b6d19baf47e7e341db84189567
iahuang <iahuang@users.noreply.github.com>
2018-07-26 15:45:02 -0400

    Lane Detection (Alpha)

--- laneDetection.py
+++ laneDetection.py
@@ -19,13 +19,16 @@ def autoCanny(image, sigma=0.33):
 
 
 def process(color):
+    color = color[color.shape[0]//2:,:]
     img = grayscale(color)
-    kernel = np.ones((3,3),np.float32)/9
+    kernel = np.ones((5,5),np.float32)/25
     img = cv2.filter2D(img,-1,kernel)
     edges = autoCanny(img)
-    lines = cv2.HoughLines(edges,1,np.pi/180,200)
+    output = color*edges.reshape([edges.shape[0],edges.shape[1],1])
+
+    lines = cv2.HoughLines(edges,1,np.pi/180,100)
     if lines is None:
-        return color
+        return output
     for line in lines:
         for rho,theta in line:
             a = np.cos(theta)
@@ -36,17 +39,26 @@ def process(color):
             y1 = int(y0 + 1000*(a))
             x2 = int(x0 - 1000*(-b))
             y2 = int(y0 - 1000*(a))
-            cv2.line(color,(x1,y1),(x2,y2),(0,255,0),2)
-    # minLineLength = 30
-    # maxLineGap = 10
-    # lines = cv2.HoughLinesP(edges,1,np.pi/180,15,minLineLength,maxLineGap)
+        
+            # m = (y2-y1)/((x2-x1))
+            # b = y1-m*x1
+
+            cv2.line(output,(x1,y1),(x2,y2),(0,255,0),2)
+            cv2.circle(output,(int(x0),int(y0)),4,(255,0,0),-1)
+    # minLineLength = 10
+    # maxLineGap = 20
+    # lines = cv2.HoughLinesP(edges,1,np.pi/180,100,minLineLength,maxLineGap)
+    # if lines is None:return output
     # for x in range(0, len(lines)):
     #     for x1,y1,x2,y2 in lines[x]:
-    #         cv2.line(color,(x1,y1),(x2,y2),(0,255,0),2)
-    return color
+    #         cv2.line(output,(x1,y1),(x2,y2),(0,255,0),2)
+    return output
 
 if __name__ == "__main__":
     cam = Camera(mirror=True)
+    time.sleep(5)
+    cv2.imwrite("test/lanes.png",process(cam.image))
+    raise SystemExit
     while 1:
         cv2.imshow('my webcam', process(cam.image))
         if cv2.waitKey(1) == 27:
