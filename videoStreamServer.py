189041b27a6115d68408d0b23cabc481c19ded2f
thatguy1234510 <theoac2009@outlook.com>
2018-07-27 02:03:00 -0400

    luh

--- videoStreamServer.py
+++ videoStreamServer.py
@@ -19,14 +19,14 @@ while True:
 cam = camera.Camera()
 
 while True:
-    b = io.BytesIO()
+    
     img=cam.image
     #not sure if np.save compression is worth io overhead...
     Tfile=TemporaryFile()
     #use numpys built in save function to convert image to bytes
     np.save(Tfile,img)
     #compress it into even less bytes
-    b.write(zstandard.ZstdCompressor.compress(b(Tfile.read(Tfile.tell()))))
+    b = io.BytesIO(zstandard.ZstdCompressor.compress(b(Tfile.read(Tfile.tell()))))
     conn.send(b.getvalue())
 
 s.close()
