06db124f0fc1e8ed53eaa5a55ca193d0ed29c39d
iahuang <iahuang@users.noreply.github.com>
2018-07-27 08:59:12 -0400

    Refactoring

--- videoStreamServer.py
+++ videoStreamServer.py
@@ -6,32 +6,31 @@ import numpy as np
 from tempfile import TemporaryFile
 import zstandard
 
-s = socket.socket()
-s.bind(('', 444))
-
-s.listen(10)
-
-while True:
-    conn, addr = s.accept()
-    print('Connected with ' + addr[0] + ':' + str(addr[1]))
-    break
-
-cam = camera.Camera()
-
-
-#not sure if np.save compression is worth io overhead...
-Tfile=TemporaryFile()
-C=zstandard.ZstdCompressor()
-while True:
-    #fetch the image
-    img=cam.image
+class Server:
+    def __init__(self,**kwargs):
+        s = socket.socket()
+        s.bind(('', kwargs.get("port",444)))
+        s.listen(10)
+        self.s = s
+        self.verbose = kwargs.get("verbose",True)
+    def serve(self):
+        while True:
+            self.conn, self.clientAddr = self.s.accept()
+            if self.verbose:
+                print('Connected with ' + self.clientAddr[0] + ':' + str(self.clientAddr[1]))
+    def startStream(self,getFrame):
+        #not sure if np.save compression is worth io overhead...
+        Tfile=TemporaryFile()
+        C=zstandard.ZstdCompressor()
+        while True:
+            #fetch the image
+            img=getFrame()
+            
+            #use numpys built in save function to convert image to bytes
+            np.save(Tfile,img)
+            #compress it into even less bytes
+            b = io.BytesIO(C.compress(Tfile.read(Tfile.tell()).encode()))
+            #send it            self.conn.send(b.getvalue())
+    def close(self):
+        self.s.close()
     
-    #use numpys built in save function to convert image to bytes
-    np.save(Tfile,img)
-    #compress it into even less bytes
-    b = io.BytesIO(C.compress(Tfile.read(Tfile.tell()).encode()))
-    #send it
-    print("sending...")
-    conn.send(b.getvalue())
-
-s.close()
