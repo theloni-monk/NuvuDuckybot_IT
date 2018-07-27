b9b3ab0d555ba333d92c4174d57a1ea78c5e6265
iahuang <iahuang@users.noreply.github.com>
2018-07-27 13:25:33 -0400

    fml

--- videoStreamServer.py
+++ videoStreamServer.py
@@ -58,7 +58,7 @@ class Server:
         self.s.close()
 
 def retrieveImage(cam):
-    image = cam.image
+    image = cv2.resize(cam.imageNoDistort,(0,0),fx=0.5,fy=0.5)
     image = laneDetection.process(image)
     return image
 
