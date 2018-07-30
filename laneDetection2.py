a3fe02da74b745d6967d04a782ecc326801ef10d
thatguy1234510 <theoac2009@outlook.com>
2018-07-30 10:51:24 -0400

    started to class-ify lane detection

--- laneDetection2.py
+++ laneDetection2.py
@@ -4,49 +4,52 @@ import numpy as np
 import time
 import colorsys
 
-class Profile:
+class ColorProfile:
     lanes = {
         "yellow":(213,177,91),
         "white":(255,255,255),
         "grey":(150,150,150)
     }
+class LaneDetector:
+    def __init__(self, **kwargs):
+        pass
 
-def calibrate(img,profile,**kwargs):
-    K = kwargs.get("K",3)
-    blurSize = kwargs.get("blurSize",(5,5))
-    w = img.shape[1]
-    h = img.shape[0]
-    img=cv2.GaussianBlur(img,blurSize,0)
-    Z = img.reshape((-1,3))
+    def calibrate(self, img, profile, **kwargs):
+        K = kwargs.get("K",3)
+        blurSize = kwargs.get("blurSize",(5,5))
+        w = img.shape[1]
+        h = img.shape[0]
+        img=cv2.GaussianBlur(img,blurSize,0)
+        Z = img.reshape((-1,3))
 
-    # convert to np.float32
-    Z = np.float32(Z)
-    newProfile = {}
-    # define criteria, number of clusters(K) and apply kmeans()
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-    ret,label,center=cv2.kmeans(Z,K,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
-    
-    chsv = np.array([colorsys.rgb_to_hsv(*(c/255)) for c in center])
+        # convert to np.float32
+        Z = np.float32(Z)
+        newProfile = {}
+        # define criteria, number of clusters(K) and apply kmeans()
+        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+        ret,label,center=cv2.kmeans(Z,K,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
+        
+        chsv = np.array([colorsys.rgb_to_hsv(*(c/255)) for c in center])
 
-    for name in profile:
-        color = np.array(colorsys.rgb_to_hsv(*np.array(profile[name])/255))
-        losses = np.abs(chsv-color).mean(axis=1)
-        n = np.argmin(losses)
-        newProfile[name] = center[n]
+        for name in profile:
+            color = np.array(colorsys.rgb_to_hsv(*np.array(profile[name])/255))
+            losses = np.abs(chsv-color).mean(axis=1)
+            n = np.argmin(losses)
+            newProfile[name] = center[n]
 
-    center = np.uint8(center)
-    res = center[label.flatten()]
-    res2 = res.reshape((img.shape))
-    return newProfile,res2
+        center = np.uint8(center)
+        res = center[label.flatten()]
+        res2 = res.reshape((img.shape))
+        return newProfile,res2
 
-def process(img):
-    return img
+    def process(self,img):
+        return img
 
-def getCalibImage(cam,iters=10):
-    img = None
-    for i in range(iters):
-        img = cam.image
-    return img
+    def getCalibImage(self,cam,iters=10):
+        img = None
+        for i in range(iters):
+            img = cam.image
+        return img
 
 if __name__ == "__main__":
     cam = Camera(mirror=True)
