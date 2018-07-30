830186c4ecce1214049be67894b47a6e8d2ecae0
Ian Huang <ianhuang@Ians-MacBook-Pro.local>
2018-07-30 13:24:14 -0400

    hmmm

--- debugLaneDetect.py
+++ debugLaneDetect.py
@@ -1,13 +1,13 @@
 from laneDetection2 import *
 from rpistream import *
 
-def makeImg(cam, detectFunc, scale, args):
+def makeImg(cam, detectFunc, scale):
     image = cv2.resize(cam.image,(0,0),fx=scale,fy=scale)
-    return detectFunc(image)
+    return detectFunc(image,ColorProfile.lanes,debug=True)
 
 Ld= LaneDetector() #needs more params
 cam=camera.Camera()
 scale=1
 server = streamserver.Server(port=5000)
 server.serve() # Blocking; waits for a connection before continuing
-server.startStream(makeImg,[cam, Ld.process, scale]) # Calls retrieveImage(*args) every frame  
+server.startStream(makeImg,[cam, Ld.calibrate, scale]) # Calls retrieveImage(*args) every frame  
