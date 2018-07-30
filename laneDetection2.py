00efcf0e0ab4ecbe38eeca7f8d69c8230fc91479
Ian Huang <ianhuang@Ians-MacBook-Pro.local>
2018-07-30 10:37:24 -0400

    fdsasadf

--- laneDetection2.py
+++ laneDetection2.py
@@ -2,6 +2,14 @@ import cv2
 from rpistream.camera import Camera
 import numpy as np
 import time
+import colorsys
+
+class Profile:
+    lanes = {
+        "yellow":(213,177,91),
+        "white":(255,255,255),
+        "grey":(150,150,150)
+    }
 
 def calibrate(img,profile,**kwargs):
     K = kwargs.get("K",5)
@@ -17,40 +25,34 @@ def calibrate(img,profile,**kwargs):
     # define criteria, number of clusters(K) and apply kmeans()
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
     ret,label,center=cv2.kmeans(Z,K,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
-
+    
+    
     for name in profile:
         color = profile[name]
         losses = np.abs(center-np.array(color)).mean(axis=1)
         n = np.argmin(losses)
         newProfile[name] = center[n]
-        
-    return newProfile
-def process(img):
-    w = img.shape[1]
-    h = img.shape[0]
-    img=cv2.GaussianBlur(img,(5,5),0)
-    Z = img.reshape((-1,3))
 
-    # convert to np.float32
-    Z = np.float32(Z)
-
-    # define criteria, number of clusters(K) and apply kmeans()
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-    K = 5
-    ret,label,center=cv2.kmeans(Z,K,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
-    print("RET: ",ret)
-    print("LBL: ",label.shape)
-    print("CTR: ",center)
-    # Now convert back into uint8, and make original image
     center = np.uint8(center)
     res = center[label.flatten()]
     res2 = res.reshape((img.shape))
-    return res2
+    return newProfile,res2
 
+def process(img):
+    return img
+
+def getCalibImage(cam,iters=10):
+    img = None
+    for i in range(iters):
+        img = cam.image
+    return img
 
 if __name__ == "__main__":
     cam = Camera(mirror=True)
+    calibImg = getCalibImage(cam)
+    profile,res = calibrate(calibImg,Profile.lanes)
+    print(profile)
     while 1:
-        cv2.imshow('my webcam', process(cam.image))
+        cv2.imshow('my webcam', res)
         if cv2.waitKey(1) == 27:
             break  # esc to quit
