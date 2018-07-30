12c8c59f742c212d591a69af29ffdb466818c7de
Ian Huang <ianhuang@Ians-MacBook-Pro.local>
2018-07-30 09:47:21 -0400

    great david just deprecated the code i was working on by telling us about this function that i didn't know about 5 seconds ago

--- laneDetection2.py
+++ laneDetection2.py
@@ -7,7 +7,7 @@ def process(img):
     w = img.shape[1]
     h = img.shape[0]
     img=cv2.GaussianBlur(img,(5,5),0)
-    img = (cv2.cvtColor(img, cv2.COLOR_BGR2HSV)/255).astype("float")
+    hsv = (cv2.cvtColor(img, cv2.COLOR_BGR2HSV)/255).astype("float")
     
     # hues = img.copy()
     # hues[:,:,1:] = 0
@@ -16,17 +16,17 @@ def process(img):
     # vals[:,:,0] = 0
     # vals[:,:,1] = 0
     # vals = np.sum(vals,2)
-    img-=np.array([40/255,1,0.8])
+    img=hsv-np.array([40/255,1,0.8])
     #img = np.abs(hues-1/38) + np.vals
     img = ((np.mean(np.abs(img),axis=2)<0.3)).astype("float")
     edges = cv2.Canny((img*255).astype("uint8"), threshold1=200, threshold2=300)
     #mask = cv2.inRange(img, lower, upper)
     #img = cv2.
     #img = cv2.cvtColor(img.astype('uint8'), cv2.COLOR_BGR2GRAY)/255
-    lines = cv2.HoughLinesP(imgg, 1, np.pi/180, 180, 20, 15)
+    #lines = cv2.HoughLinesP(img, 1, np.pi/180, 180, 20, 15)
     
     
-    return edges
+    return hsv[:,:,2]
 
 if __name__ == "__main__":
     cam = Camera(mirror=True)
