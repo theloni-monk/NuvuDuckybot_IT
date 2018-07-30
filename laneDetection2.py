5caf6b0d0cdb7195935e22c57657c0dc11ff2fec
Ian Huang <ianhuang@Ians-MacBook-Pro.local>
2018-07-30 11:25:21 -0400

    dsaflkjds

--- laneDetection2.py
+++ laneDetection2.py
@@ -3,7 +3,8 @@ from rpistream.camera import Camera
 import numpy as np
 import time
 import colorsys
-
+def normLayer(l):
+    return (l-np.min(l))/(np.max(l)-np.min(l))
 class ColorProfile:
     lanes = {
         "yellow":(213,177,91),
@@ -48,8 +49,18 @@ class LaneDetector:
             return res2
 
     def process(self,img):
-
-        return img
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        l = []
+        for name in profile:
+            color = np.array(colorsys.rgb_to_hsv(*np.array(profile[name])))
+            losses = np.abs(hsv-color).mean(axis=2)
+            l.append(losses.reshape(img.shape[0],img.shape[1],1))
+        print(np.mean(l[0]-l[1]))
+        l = np.concatenate((l[0],l[1],l[2]),axis=2)
+        print(l[0,1])
+        #return normLayer(l)
+        l = np.argmin(l,2)
+        return (l==0).astype("float")
 
     def getCalibImage(self,cam,iters=10):
         img = None
@@ -60,11 +71,11 @@ class LaneDetector:
 if __name__ == "__main__":
     cam = Camera(mirror=True)
     LD=LaneDetector()
-    profile=ColorProfile()
+    profile=ColorProfile.lanes
     calibImg = LD.getCalibImage(cam)
     res=LD.calibrate(calibImg, profile, debug=True)
     print(LD.KmeansProfile)
     while 1:
-        cv2.imshow('my webcam', res)
+        cv2.imshow('my webcam', LD.process(cam.image))
         if cv2.waitKey(1) == 27:
             break  # esc to quit
