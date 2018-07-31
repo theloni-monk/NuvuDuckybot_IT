4373afdf454c305eefe048cb3b0280ca7a6880f5
Ian Huang <ianhuang@dhcp-18-111-17-55.dyn.MIT.EDU>
2018-07-31 12:41:25 -0400

    my code

--- laneDetection2.py
+++ laneDetection2.py
@@ -7,33 +7,68 @@ import sys
 from sklearn import svm
 import pickle
 
+
 def normLayer(l):
     return (l-np.min(l))/(np.max(l)-np.min(l))
 
+
 class ColorProfile:
     lanes = {
-        "yellow":(213,177,50), #rgb
-        "white":(255,255,255),
-        "grey":(150,150,150)
+        "yellow": (213, 177, 50),  # rgb
+        "white": (255, 255, 255),
+        "grey": (150, 150, 150)
     }
 
-class Pspec:
+
+class Persp:
     horizonOffset = -100
     HhorizonOffset = 100
+
     def getDefault(img):
         height = img.shape[0]
         width = img.shape[1]
         return [
             (0, height),
-            (width//2 - Pspec.HhorizonOffset, height//2+Pspec.horizonOffset),
-            (width//2 + Pspec.HhorizonOffset, height//2+Pspec.horizonOffset),
+            (width//2 - Persp.HhorizonOffset, height//2+Persp.horizonOffset),
+            (width//2 + Persp.HhorizonOffset, height//2+Persp.horizonOffset),
             (width, height)
         ]
-    
-    def unWarp(img, perpVerts,sizex=200,sizey=200):
-        dst = np.array([[0,0],[sizex,0],[sizex,sizey],[0,sizey]])
-        M = cv2.getPerspectiveTransform(perpVerts,dst)
+
+    def unWarp(self,img, perpVerts, sizex=200, sizey=200):
+        dst = np.array([[0, 0], [sizex, 0], [sizex, sizey], [0, sizey]])
+        M = cv2.getPerspectiveTransform(perpVerts, dst)
         return cv2.warpPerspective(img, M)
+
+
+def grayscale(img): return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+
+def region_of_interest(img, vertices):
+    mask = np.zeros_like(img)
+    match_mask_color = 255
+    cv2.fillPoly(mask, np.int32([vertices]).astype("int32"), (255, 255, 255))
+    masked_image = cv2.bitwise_and(img, mask)
+    return masked_image
+
+
+def autoCanny(image, sigma=0.33):
+    # compute the median of the single channel pixel intensities
+    v = np.median(image)
+
+    # apply automatic Canny edge detection using the computed median
+    lower = int(max(0, (1.0 - sigma) * v))
+    upper = int(min(255, (1.0 + sigma) * v))
+    edged = cv2.Canny(image, lower, upper)
+
+    # return the edged image
+    return edged
+
+
+def unzero(x):
+    if x == 0:
+        x = 0.0001
+    return x
+
 
 class LaneDetector:
     def __init__(self, **kwargs):
@@ -43,68 +78,73 @@ class LaneDetector:
         self.calibrated = False
         self.clf = None
 
-    def getCalibImage(self,cam,iters=10):
+    def getCalibImage(self, cam, iters=10):
         img = None
         for i in range(iters):
             img = cam.image
         return img
 
     def calibrateKmeans(self, img, profile, **kwargs):
-        
-        K = kwargs.get("K",5)
-        debug=kwargs.get("debug",False)
-        blurSize = kwargs.get("blurSize",(5,5))
+
+        K = kwargs.get("K", 5)
+        debug = kwargs.get("debug", False)
+        blurSize = kwargs.get("blurSize", (5, 5))
         w = img.shape[1]
         h = img.shape[0]
-        img=cv2.GaussianBlur(img,blurSize,0)
-        Z = img.reshape((-1,3))
+        img = cv2.GaussianBlur(img, blurSize, 0)
+        Z = img.reshape((-1, 3))
 
         # convert to np.float32
         Z = np.float32(Z)
         kProfile = {}
         kLabels = {}
         kNames = {}
-        stepSize = kwargs.get("stepSize",5)
+        stepSize = kwargs.get("stepSize", 5)
 
         # define criteria, number of clusters(K) and apply kmeans()
-        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-        ret,labels,center=None,None,None
+        criteria = (cv2.TERM_CRITERIA_EPS +
+                    cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+        ret, labels, center = None, None, None
         if sys.version_info[0] == 3:
-            ret,labels,center=cv2.kmeans(Z,K,None, criteria,10,cv2.KMEANS_RANDOM_CENTERS)
+            ret, labels, center = cv2.kmeans(
+                Z, K, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
         else:
-            ret,labels,center=cv2.kmeans(Z,K,criteria,10, cv2.KMEANS_RANDOM_CENTERS)
+            ret, labels, center = cv2.kmeans(
+                Z, K, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
 
-        chsv = np.array([colorsys.rgb_to_hsv(*(c[::-1]/255)) for c in center]) # Center colors as HSV
+        chsv = np.array([colorsys.rgb_to_hsv(*(c[::-1]/255))
+                         for c in center])  # Center colors as HSV
 
-        Count=0
+        Count = 0
         for name in profile:
 
-            color = np.array(colorsys.rgb_to_hsv(*(np.array(profile[name])/255))) # Profile color as HSV
-            losses = np.abs(chsv-color).mean(axis=1) # Color diffs
-            n = np.argmin(losses) # Find closest center color to profile color
+            color = np.array(colorsys.rgb_to_hsv(
+                *(np.array(profile[name])/255)))  # Profile color as HSV
+            losses = np.abs(chsv-color).mean(axis=1)  # Color diffs
+            n = np.argmin(losses)  # Find closest center color to profile color
             kProfile[name] = chsv[n]
             kLabels[n] = name
             kNames[name] = n
-        
+
         center = np.uint8(center)
-        
+
         res = center[labels.flatten()]
         res2 = res.reshape((img.shape))
 
-        labels = labels.reshape((img.shape[0],img.shape[1]))
-        
+        labels = labels.reshape((img.shape[0], img.shape[1]))
+
         trainX = []
         trainY = []
-        for x in range(0,labels.shape[1],stepSize):
-            for y in range(0,labels.shape[0],stepSize):
-                label = labels[y,x]
+        for x in range(0, labels.shape[1], stepSize):
+            for y in range(0, labels.shape[0], stepSize):
+                label = labels[y, x]
                 if not label in kLabels:
                     continue
-                trainX.append(img[y,x])
+                trainX.append(img[y, x])
                 trainY.append(label)
-        trainX,trainY = np.array(trainX),np.array(trainY)
+        trainX, trainY = np.array(trainX), np.array(trainY)
         print(kLabels)
-        print("Training size:",trainX.size)
+        print("Training size:", trainX.size)
         self.clf = svm.LinearSVC()
         self.clf.fit(trainX, trainY)
 
@@ -117,70 +157,32 @@ class LaneDetector:
         if debug:
             return res2
 
-
-
-
-    def grayscale(img): return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-
-    def region_of_interest(img, vertices):
-        mask = np.zeros_like(img)
-        match_mask_color = 255
-        cv2.fillPoly(mask, np.int32([vertices]).astype("int32"), (255, 255, 255))
-        masked_image = cv2.bitwise_and(img, mask)
-        return masked_image
-
-
-    def autoCanny(image, sigma=0.33):
-        # compute the median of the single channel pixel intensities
-        v = np.median(image)
-
-        # apply automatic Canny edge detection using the computed median
-        lower = int(max(0, (1.0 - sigma) * v))
-        upper = int(min(255, (1.0 + sigma) * v))
-        edged = cv2.Canny(image, lower, upper)
-
-        # return the edged image
-        return edged
-
-
-    def unzero(x):
-        if x == 0:
-            x = 0.0001
-        return x
-
-    def process3(self,imgin):
+    def process3(self, imgin):
         #img = cv2.resize(img,(0,0),fx=0.5,fy=0.5)
         shape = imgin.shape
         pixels = shape[0]*shape[1]
-        height = color.shape[0]
-        width = color.shape[1]
-        horizonOffset = -100
-        HhorizonOffset = 100
-        region_of_interest_vertices = [
-            (0, height),
-            (width//2 - HhorizonOffset, height//2+horizonOffset),
-            (width//2 + HhorizonOffset, height//2+horizonOffset),
-            (width, height),
-        ]
-        colors=["yellow","white"]
+        clipping = Persp.getDefault(imgin)
+        colors = ["yellow", "white"]
 
-        for CurrColor in colors:
-            debugOut=imgin
+        for currColor in colors:
+            debugOut = imgin 
 
-            #svm classification:
-            bools = (self.clf.predict(img.reshape(pixels,3)).reshape((shape[0],shape[1],1))==self.kNames[color]).astype("float")
-            boolimg = bools.astype("uint8")*255 
+            # svm classification:
+            bools = (self.clf.predict(imgin.reshape(pixels, 3)).reshape(
+                (shape[0], shape[1], 1)) == self.kNames[currColor]).astype("float")
+            boolimg = bools.astype("uint8")*255
+
+            # crop->grayscale->gaussblur->canny
+            boolimg = autoCanny(boolimg)
+            cropped = region_of_interest(boolimg, np.array(
+                [clipping], np.int32))
+            cropped = cropped.astype("uint8")
+            #img = cv2.GaussianBlur(cropped, (5, 5), 0)
             
-            #crop->grayscale->gaussblur->canny
-            cropped = region_of_interest(color, np.array([region_of_interest_vertices], np.int32))
-            img = grayscale(cropped)
-            img = cv2.GaussianBlur(img, (5, 5), 0)
-            edges = autoCanny(img)
 
-            #detect lines
+            # detect lines
             lines = cv2.HoughLines(edges, 1, np.pi/180, 175)
-
+            return edges
             if lines is None:
                 print("no lines found")
                 return debugOut
@@ -198,35 +200,34 @@ class LaneDetector:
 
                     m = unzero((y2-y1)/(unzero(x2-x1)))
                     b = y1-m*x1
-                    lineColor = CurrColor
+                    lineColor = currColor
 
-                    cv2.line(output, (0, int(b)),
-                            (1000, int(m*1000+b)), tuple(lineColor), 3)
-                    cv2.circle(output, (int(x0), int(y0)), 4, (255, 0, 0), -1)
-
+                    cv2.line(debugOut, (0, int(b)),
+                             (1000, int(m*1000+b)), tuple(lineColor), 3)
+                    cv2.circle(debugOut, (int(x0), int(y0)), 4, (255, 0, 0), -1)
+            
         return debugOut
-
-
 
     def loadSvm(self, path):
         with open(path, 'rb') as fid:
-            self.clf=pickle.load(fid)
-    
+            self.clf = pickle.load(fid)
+
     def saveSvm(self, path):
         with open(path, 'wb') as fid:
-            pickle.dump(self.clf, fid)  
+            pickle.dump(self.clf, fid)
 
     def log(self, m):
         if self.verbose:
             print(m)  # printout if verbose
 
+
 if __name__ == "__main__":
     cam = Camera(mirror=True)
-    LD=LaneDetector()
-    p=ColorProfile.lanes
+    LD = LaneDetector()
+    p = ColorProfile.lanes
     calibImg = LD.getCalibImage(cam)
-    res=LD.calibrateKmeans(calibImg, p, debug=True, stepSize=20)
-    #print(LD.kProfile)
+    res = LD.calibrateKmeans(calibImg, p, debug=True, stepSize=20)
+    # print(LD.kProfile)
     while 1:
         cv2.imshow('my webcam', LD.process3(cam.image))
         if cv2.waitKey(1) == 27:
