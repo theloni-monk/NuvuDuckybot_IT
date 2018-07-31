da092e7c8bfd411754ba3af26fe8c6f8a71dbd95
thatguy1234510 <theoac2009@outlook.com>
2018-07-31 14:06:56 -0400

    pu

--- debugClient.py
+++ debugClient.py
@@ -1,5 +1,5 @@
 from rpistream.streamclient import *
 rpi = "18.111.87.85"
 ian = "10.189.75.150"
-client = Client(serverIp=rpi, port = 5000) # Connects to the server
+clearclient = Client(serverIp=rpi, port = 5000) # Connects to the server
 client.startStream() # Starts recieving data and displaying the video
