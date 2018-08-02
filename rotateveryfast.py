a4dffe01829bcf52754c383643d39f4ace925fa1
Ian Huang <ianhuang46@gmail.com>
2018-08-02 11:06:55 -0400

    fads

--- rotateveryfast.py
+++ rotateveryfast.py
@@ -1,10 +1,11 @@
 from driver import Driver # use the legacy module it's pretty great
 from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
+import time
 if __name__ == "__main__":
     driver = Driver()
-    
+    start = time.time()
     while 1:
-        print("Running...")
+        print("time elapsed: "+str(time.time()-start))
         motor = driver.lmotor
         motor.run(Adafruit_MotorHAT.FORWARD)
         motor.setSpeed(255)
