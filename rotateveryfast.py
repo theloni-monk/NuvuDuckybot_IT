d81d01fe4fecf90f0f69d2b58d22059274974a86
Ian Huang <ianhuang46@gmail.com>
2018-08-02 10:47:14 -0400

    fa

--- rotateveryfast.py
+++ rotateveryfast.py
@@ -2,11 +2,11 @@ from driver import Driver # use the legacy module it's pretty great
 from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
 if __name__ == "__main__":
     driver = Driver()
-    motor = driver.lmotor
-    motor.run(Adafruit_MotorHAT.FORWARD)
-    motor.setSpeed(255)
-    motor = driver.rmotor
-    motor.run(Adafruit_MotorHAT.BACKWARD)
-    motor.setSpeed(255)
+    
     while 1:
-        pass
+        motor = driver.lmotor
+        motor.run(Adafruit_MotorHAT.FORWARD)
+        motor.setSpeed(255)
+        motor = driver.rmotor
+        motor.run(Adafruit_MotorHAT.BACKWARD)
+        motor.setSpeed(255)
